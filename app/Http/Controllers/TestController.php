ac32ad3b51f69a3137c39d495b8df5a037730ee1
Luca <luca@brokenbox.io>
2020-08-08 21:03:29 +0200

    recursion green

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class TestController extends Controller
 {
     public function recursion(Request $request) {
+        $data = $request->validate([
+            'string' => 'required|string'
+        ]);
+
         return [
-            'string' => 'cba'
+            'string' => $this->reverseString(str_split($data["string"]))
         ];
+    }
+
+    private function reverseString($aStr) {
+        if(count($aStr) == 1) {
+            return $aStr[0];
+        }
+
+        return $this->reverseString(array_splice($aStr, 1)) . $aStr[0];
     }
 }
