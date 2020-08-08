07d6d69128d4d92fd1e111fbba5e35f944d2fd42
Luca <luca@brokenbox.io>
2020-08-08 20:27:51 +0200

    test recursion green

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function recursion(Request $request) {
-
+        return [
+            'string' => 'cba'
+        ];
     }
 }
